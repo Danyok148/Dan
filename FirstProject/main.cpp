44f5aa0e44891b1ee312217ca1da1de93d10db65
Danyok148 <berezhnyidanulo@gmail.com>
2025-05-15 18:18:56 +0300

    Add project files.

---
+++ FirstProject/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ const int size = 5;
+ int arr[size] = { 1,2,3,4,5 };
+
+ const int ROW = 2, COL = 5;
+ int array2d[ROW][COL] = { {1,2,3,4,5},{6,7,8,9,10} };
+
+ printf("El: %i\n", array2d[ROW - 1][COL - 1]);
+
+ return 0;
+}
